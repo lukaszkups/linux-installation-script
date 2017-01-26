1bd28b58c9cbf6b751035d83a3ba257b7385b9a7
lkups <l.kups@itm.biz.pl>
2017-01-26 07:55:15 +0100

    updated install script with new features

---
+++ README.md
@@ -0,0 +1,4 @@
+# linux installation script
+
+This is my installation script after system format - it install all necessary tools for me from scratch.
+
--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
-echo -e "===============================\n"
-echo -e "|installation will now proceed|\n"
-echo -e "===============================\n"
-echo -e "\n"
-echo -e "Your e-mail address?\n"
+echo "===============================\n"
+echo "|installation will now proceed|\n"
+echo "===============================\n"
+echo "\n"
+echo "Your e-mail address?\n"
 read email
-echo -e "\n"
-echo -e "Your name?\n"
+echo "\n"
+echo "Your name?\n"
 read name
-echo -e "\n"
+echo "\n"
 
 # install ppa
 sudo apt-get install software-properties-common
@@ -23,6 +23,8 @@ sudo apt-get install software-properties-common
   sudo add-apt-repository ppa:otto-kesselgulasch/gimp
   # inkscape
   sudo add-apt-repository ppa:inkscape.dev/stable
+  # sublime text 3
+  sudo add-apt-repository ppa:webupd8team/sublime-text-3
 
 # update system ppa repositories list
 sudo apt-get update
@@ -34,13 +36,22 @@ sudo apt-get install git
 sudo apt-get install firefox
 
 # install vim
-sudo apt-get install vim
+  sudo apt-get install vim
+  # create necessary folders for my customized vimrc config
+  mkdir ~/.vim/backups 
+  mkdir ~/.vim/backups/tmp
+  mkdir ~/.vim/swaps
+  mkdir ~/.vim/swaps/tmp
+  mkdir ~/.vim/tmp
 
 # install elementary tweaks
 sudo apt-get install elementary-tweaks
 
 # install numix icons
 sudo apt-get install numix-gtk-theme numix-icon-theme-circle
+
+# install deb packages support
+sudo apt-get install gdebi
 
 # install guake
 sudo apt-get install guake 
@@ -56,8 +67,18 @@ sudo apt-get install inkscape
 
 # config git
 git config --global user.email $email
-git config --global user.name $name
 
+# install node version manager
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+source ~/.bashrc
+source ~/.zshrc
+
+#install node using nvm
+nvm install node
+nvm use node
+
+# install postgres
+sudo apt-get install postgresql postgresql-contrib
 
 # install hipchat
   sudo sh -c 'echo "deb https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client $(lsb_release -c -s) main" > /etc/apt/sources.list.d/atlassian-hipchat4.list'
@@ -68,22 +89,35 @@ git config --global user.name $name
 # install xclip, needed for next step
 sudo apt-get install xclip
 
+# install Sublime Text 3
+sudo apt-get install sublime-text-installer
+
+# install zsh
+sudo apt-get install zsh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # generate keygen, needed for git
   ssh-keygen -t rsa -b 4096 - C $email
   # start ssh-agent if not running
   eval "$(ssh-agent -s)"
+  # generate key
+  ssh-keygen -t rsa
   # add ssh key to ssh-agent
   ssh-add ~/.ssh/id_rsa
   # copy ssh key to clipboard
   xclip -sel clip < ~/.ssh/id_rsa.pub
-  echo -e "You have now public ssh key in Your clipboard - add it to Your github account\n"
+  echo "You have now public ssh key in Your clipboard - add it to Your github account\n"
 
-echo -e "Updating installed software...\n"
+echo "Updating installed software...\n"
 # update installed software
 sudo apt-get upgrade
-echo -e "Congratulations! Everything has been installed!"
-echo -e "====================\n"
-echo -e "|other handy links:|\n"
-echo -e "====================\n"
-echo -e "\n"
-echo -e "Intel XDK: https://software.intel.com/en-us/intel-xdk\n"
+echo "Congratulations! Everything has been installed!"
+echo "====================\n"
+echo "|other handy links:|\n"
+echo "====================\n"
+echo "\n"
+echo "Intel XDK: https://software.intel.com/en-us/intel-xdk\n"
+echo "vimrc repo: https://github.com/lukaszkups/vimrc\n" 
+echo "Creating Postgres Database: https://github.com/lukaszkups/lukaszkups-old/blob/master/content/blog/0019_things_todo_after_installinig_elementaryos.md\n"
